d422aee5e183032cc5df715c0685b9824e6bc145
Tangworld <948138224@qq.com>
2016-08-12 20:55:57 +0800

    tsj2016081202

--- src/main/java/com/busasst/controller/UserController.java
+++ src/main/java/com/busasst/controller/UserController.java
@@ -1,5 +1,4 @@
 package com.busasst.controller;
-
 import com.busasst.dao.RouteDao;
 import com.busasst.dao.UserDao;
 import com.busasst.entity.AdminEntity;
@@ -10,39 +9,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
-
 /**
  * Created by tsj on 16-8-10.
  */
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
     @Autowired
     @Qualifier("routeDao")
     private RouteDao routeDao;
-
     @Autowired
     @Qualifier("userDao")
     private UserDao userDao;
-
-
-
     @RequestMapping(value = "/changetoregist", method = RequestMethod.GET)
     public String changeToRegist() {
         return "regist";
     }
-
     @RequestMapping(value = "/changetologin", method = RequestMethod.GET)
     public String changeToLogin() {
         return "redirect:/index";
     }
-
-    @RequestMapping(value = "/login", method = RequestMethod.POST)           //µ÷ÓÃpost·½·¨
+    @RequestMapping(value = "/login", method = RequestMethod.POST) //è°ƒç”¨postæ–¹æ³•
     public String login(HttpServletRequest request, String username,
                         String password, String codenum,HttpSession session) {
         int status = 0;
@@ -50,62 +40,53 @@ public class UserController {
         String usernmaemessage = new String();
         String passwordmessage = new String();
         String codemessage = new String();
-
         if (username.isEmpty()) {
-            usernmaemessage = "ÇëÊäÈëÓÃ»§Ãû";
+            usernmaemessage = "è¯·è¾“å…¥ç”¨æˆ·å";
         } else {
             AdminEntity user = userDao.getByName(username);
             if(user==null){
-                usernmaemessage = "²»´æÔÚµÄÓÃ»§£¡";
+                usernmaemessage = "ä¸å­˜åœ¨çš„ç”¨æˆ·ï¼";
             }
             else if (password.isEmpty()) {
-                passwordmessage = "ÇëÊäÈëÃÜÂë";
+                passwordmessage = "è¯·è¾“å…¥å¯†ç ";
             } else if (!user.getPassword().equals(password)) {
-                passwordmessage = "ÃÜÂë´íÎó";
+                passwordmessage = "å¯†ç é”™è¯¯";
             }
             else if(!codenum.equalsIgnoreCase(session.getAttribute("code").toString())){
-                codemessage = "ÑéÖ¤Âë´íÎó";
+                codemessage = "éªŒè¯ç é”™è¯¯";
             }
             else {
                 status = 1;
-                usernmaemessage = "ÓÃ»§µÇÂ½³É¹¦";
+                usernmaemessage = "ç”¨æˆ·ç™»é™†æˆåŠŸ";
                 session.setAttribute("loginUser", user);
                 session.setAttribute("userid",user.getAdminId());
                 System.out.println("message1 : " + usernmaemessage);
                 request.getSession().setAttribute("username", username);
                 System.out.println("message : " + usernmaemessage);
-
-
-
-                // if(user.getAuthority().equals("1"))return "redirect:/admin/index";
+// if(user.getAuthority().equals("1"))return "redirect:/admin/index";
                 return "cars-mng";
             }
         }
         session.setAttribute("usernmaemessage",usernmaemessage);
         session.setAttribute("passwordmessage",passwordmessage);
         session.setAttribute("codemessage",codemessage);
-
         System.out.println("usernmaemessage : " + usernmaemessage);
         System.out.println("passwordmessage : " + passwordmessage);
         System.out.println("codemessage : " + codemessage);
-
         return "redirect:/login";
     }
-
-
     @RequestMapping(value = "/regist", method = RequestMethod.POST)
     public String regist(HttpSession httpSession, String username, String password, String password2) {
         String reusernmaemessage = new String();
         String repasswordmessage = new String();
         String recodemessage = new String();
-
         if (username.isEmpty()) {
-            reusernmaemessage = "ÇëÊäÈëÓÃ»§Ãû";
+            reusernmaemessage = "è¯·è¾“å…¥ç”¨æˆ·å";
         } else if (password.isEmpty()) {
-            repasswordmessage = "ÇëÊäÈëÃÜÂë";
+            repasswordmessage = "è¯·è¾“å…¥å¯†ç ";
         } else {
             if (!password.equals(password2)) {
-                recodemessage = "Çë±£³ÖÃÜÂëºÍÈ·ÈÏÃÜÂëÒ»ÖÂ£¡";
+                recodemessage = "è¯·ä¿æŒå¯†ç å’Œç¡®è®¤å¯†ç ä¸€è‡´ï¼";
             } else {
                 AdminEntity testuser = userDao.getByName(username);
                 if(testuser==null){
@@ -115,7 +96,7 @@ public class UserController {
                     httpSession.setAttribute("username",username);
                     return "cars-mng";
                 } else{
-                    reusernmaemessage = "ÒÑ¾­´æÔÚµÄÓÃ»§Ãû£¡";
+                    reusernmaemessage = "å·²ç»å­˜åœ¨çš„ç”¨æˆ·åï¼";
                 }
             }
         }
@@ -124,26 +105,21 @@ public class UserController {
         httpSession.setAttribute("recodemessage",recodemessage);
         System.out.println("UserController : "+username);
         httpSession.setAttribute("username",username);
-
         System.out.println("reusernmaemessage:" + reusernmaemessage);
         System.out.println("repasswordmessage:" + repasswordmessage);
         System.out.println("recodemessage:" + recodemessage);
-
         return "redirect:/user/changetoregist";
-//        return "redirect:/index";
+// return "redirect:/index";
     }
-
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logout(HttpSession httpSession){
         httpSession.invalidate();
         return "redirect:/index";
     }
-
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test() {
         return "lineManage";
     }
-
     @RequestMapping(value = "/linestation", method = RequestMethod.GET)
     public String test2(Model model) {
         List<RouteEntity> routes = routeDao.getAllRoutes();
